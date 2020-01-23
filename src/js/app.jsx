4736409ef1078b7a82b974cef54c84152ecc2f29
Mike Murray <MikeMurrayDev@gmail.com>
2020-01-22 17:33:40 -0800

    Adds several packages, adds styling, adds basic template, configures parcel.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,17 +1,147 @@
 import React, { Component } from 'react';
-import './css/style.css';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FaEnvelopeSquare } from 'react-icons/fa';
+import { FaWindowClose } from 'react-icons/fa';
+import { FaGithubSquare } from 'react-icons/fa';
 
-
-
+let projectData = require('./projects.json')
 
 class App extends Component {
     constructor(props) {
       super(props);
   
-      this.state = {
+    this.state = {
+        projectsArray: [],
+        modalIsOpen: false,
+        visibleArray: []
+    };
 
-      };
-    }
+    this.makeVisible = this.makeVisible.bind(this);
+    this.hideVisibility = this.hideVisibility.bind(this);
+    this.isArrow = this.isArrow.bind(this);
+    this.ref1 = React.createRef();
+    this.ref2 = React.createRef();
+    this.ref3 = React.createRef();
+    this.ref4 = React.createRef();
+    this.ref5 = React.createRef();
+    this.ref6 = React.createRef();
+    this.ref7 = React.createRef();
+    };
+
+    componentDidMount () {
+      let refArray = [this.ref1, this.ref2, this.ref3, this.ref4, this.ref5, this.ref6, this.ref7];
+      
+      for (let i = 0; i < projectData.length; i++) {
+          projectData[i].ref = refArray[i];
+          console.log(projectData[i]);
+          console.log(refArray);
+      }
+      this.setState({projectsArray: projectData})
+      // console.log(this.state.projectsArray);
+  };
+
+  makeVisible (eachKey, eachRef) {
+      let projects = this.state.projectsArray;
+      let visible = this.state.visibleArray;
+      for (let i = 0; i < projects.length; i++) {
+          
+          if (projects[i].key === eachKey) {
+              projects[i].isVisible = true;
+              visible.push(projects[i]);
+          }
+          this.setState({
+              projectsArray: projects,
+              visibleArray: visible
+          })
+      }
+
+      
+  };
+
+  hideVisibility(eachKey) {
+      let projects = this.state.projectsArray;
+      let visible = this.state.visibleArray;
+      for (let i = 0; i < projects.length; i++) {
+          if (projects[i].key === eachKey) {
+              projects[i].isVisible = false
+          }
+          this.setState({
+              projectsArray: projects
+          })
+      }
+      for (let j = 0; j < visible.length; j++) {
+          if (visible[j].key === eachKey) {
+              visible.splice(j, 1)
+          }
+          this.setState({
+              visibleArray: visible
+          })        
+      }
+  };
+
+  isArrow () {
+      if (this.state.visibleArray.length > 0) {
+          return "arrow-down"
+      } 
+  };
+
+  render () {
+      return (
+          <>
+              <div className="contact-container">
+                  <div className="each-contact"><a href="https://www.linkedin.com/in/mikemurraydev/" target="_blank" title="LinkedIn" className="linkedin" ><FaLinkedinIn /></a></div>
+                  <div className="each-contact"><a href="mailto:mikemurraydev@gmail.com" target="_blank" title="email" className="email" ><FaEnvelopeSquare /></a></div>
+                  <div className="each-contact"><a href="https://github.com/mikemurraydev?tab=repositories" target="_blank" title="github-top" className="github-top" ><FaGithubSquare /></a></div>
+                  <div className="each-contact"><a href="https://drive.google.com/file/d/1adI01THd55IVlqbxiNmRV--xMshGa4Eq/view?usp=sharing" target="_blank" title="resume" className="resume" >Resume</a></div>
+              </div>
+              <div className="flex-container-vertical">
+                  <img src={ require("../assets/me-face.png") } className="portrait-img"/>
+                  <h1 className="name-text">Hello, I'm <span id="mike">Mike</span>. I'm a Fullstack Developer living in San Diego.</h1>
+              </div>
+
+              <div className="flex-container-row">
+              {
+                  this.state.projectsArray.map((each) => {
+                  return   <div key={ each.key }>
+                              <div className="card" onClick={ () => this.makeVisible(each.key, each.ref) }>
+                                  <div className="card-circle">
+                                      <p className="title">{ each.title }</p>
+                                  </div>
+                              </div>
+                          </div>
+                  })
+              }
+              </div>
+              <div className={ this.isArrow() }></div>
+              {
+                  this.state.visibleArray.map((each) => {
+                      return(         
+                              <div className="project-visibility-container" id={ each.key } key={ each.key } ref={ each.ref }>
+                                  <FaWindowClose className="x" onClick={() => this.hideVisibility(each.key) }/>
+                                  <div className="project-info-container">
+                                      <div className="project-title-container">
+                                          <h4 className="project-title" >{ each.title }</h4>
+                                          <p className="project-description">{ each.description }</p>
+                                          <div className="link-flex">
+                                              {each.github ? <a href={ each.github } target="_blank" className="link-button" >Github</a> : "" }
+                                              { each.hosted ? <a href={ each.hosted } target="_blank" className="link-button">View Site</a> : "" }
+                                          </div>
+                                      </div>
+                                  </div>
+                                  <div className="image-flex-container">
+                                      {
+                                          each.images.map((eachImage) => {
+                                              return <img src={ eachImage.image } key={ eachImage.key } className="each-image"/>
+                                          })
+                                      }
+                                  </div>
+                              </div>
+                      )
+                  })
+              }
+          </>
+      );
+  }
 }
 
 export default App;
